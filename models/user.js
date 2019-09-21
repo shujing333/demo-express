4ebb7ec66bd97b134ed70b12a705977201bba434
hsj <1059726649@qq.com>
2019-09-21 09:57:30 +0800

    '显示用户头像'

--- models/user.js
+++ models/user.js
@@ -1,17 +1,21 @@
 //user表的model文件
 
 //1.引入mongoose,是已经链接了mongodb的mongoose
-const mongoose=require('../config/db');
+const mongoose = require("../config/db");
 //2.定义好你要操作的表的数据结构 schema
-const schema=new mongoose.Schema({
-    //表的数据结构描述
-    username:String,
-    email:String,
-    password:String
+const schema = new mongoose.Schema({
+  //表的数据结构描述
+  username: String,
+  email: String,
+  password: String,
+  avatar: {
+    type: String,
+    default: "http://localhost:3000/tem2.jpg" //默认值
+  }
 });
 
 //3.生成model
-const model=mongoose.model('user',schema);
+const model = mongoose.model("user", schema);
 
 //4.暴露model
-module.exports=model;
+module.exports = model;
--- routes/file.js
+++ routes/file.js
@@ -1,7 +1,49 @@
 const express = require("express");
+const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
 const router = express.Router();
+const upload = multer({
+  dest: "c:/tmp"
+});
 
+//文件上传页面
 router.get("/create", (req, res) => {
   res.render("files/create");
 });
+
+//文件上传处理,上传一张的情况
+router.post("/store", upload.single("file"), (req, res) => {
+  //console.log(req.body); //{}
+  //console.log(req.file);
+  //1.通过req.file可以查看当前文件的信息
+
+  //2.需要将文件移动到当前项目的 public/upload文件夹中
+
+  //2.1先确定要放到public/upload文件夹下的文件的名字
+  let newFileName = new Date().getTime() + "_" + req.file.originalname;
+  let newPath = path.resolve(__dirname, `../public/upload/${newFileName}`);
+  //2.2使用fs.readFileSync去读取上传的文件的信息
+  let fileData = fs.readFileSync(req.file.path);
+  //2.3使用fs.writeFileSync去写文件
+  fs.writeFileSync(newPath, fileData);
+  //2.4给浏览器输出当前文件的url的访问地址
+  res.send(`http://localhost:3000/upload/${newFileName}`);
+
+  // res.send("hello");
+});
+
+//文件上传处理,多张情况
+router.post("/hello", upload.array("file", 2), (req, res) => {
+  console.log(req.files); //[{},{}]
+});
+
+//文件上传处理,多张,不同字段名的情况
+router.post(
+  "/world",
+  upload.fields([{ name: "pic1", maxCount: 1 }, { name: "pic2", maxCount: 1 }]),
+  (req, res) => {
+    console.log(req.files);
+  }
+);
 module.exports = router;
